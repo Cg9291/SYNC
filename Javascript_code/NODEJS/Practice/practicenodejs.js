2bbe9a6cec5cc265b7ccbfe5012655f47caf54c0
carlg <carlg@allrightladder.com>
2022-06-30 12:51:34 -0700

    created a practice Nodejs file-implemented http server+fs module

---
+++ Javascript_code/NODEJS/Practice/practicenodejs.js
@@ -0,0 +1,18 @@
+var http=require("http");
+var fs=require("fs");
+
+
+http.createServer(function(req,res){
+    fs.readFile('practicenodejs.html',function(err, data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080)
+
+
+
+/*res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(req.url)
+    res.write("You have found the way,good job Mando");
+    res.end()*/
