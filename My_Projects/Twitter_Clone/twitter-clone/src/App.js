fdf455ebd30500b4c04c07442eec9b0bcff5e6a5
CarlG <carl91y@gmail.com>
2022-07-06 11:54:51 -0700

    added icon row including tweet button + fixed the colors and witdh of borders to better match those of twitter

--- My_Projects/Twitter_Clone/twitter-clone/src/App.js
+++ My_Projects/Twitter_Clone/twitter-clone/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.scss';
-
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import {Helmet} from "react-helmet";//this allows to use helmet(which supports the insertion of script tags in the component's html)
+import squarepicicon from "./twitter-newui-iconkit/icons to be used/twitter-image.png"
+import gificon from "./twitter-newui-iconkit/icons to be used/twitter-gif.png"
+import alignicon from "./twitter-newui-iconkit/icons to be used/twitter-align.png"
+import locationicon from "./twitter-newui-iconkit/icons to be used/twitter-location.png"
+import noiconicon from "./twitter-newui-iconkit/icons to be used/twitter-cross-2.png"
+
+
+
+
 
 function App() {
   return (
@@ -78,6 +86,17 @@ function Timeline(props){
       <div id="tweet-area">
         <img src="https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png?w=640"/>
         <input id="tweet-input" placeholder="What's happening?"></input>
+      </div>
+      <div id="icons-row">
+        <ul>
+          <li><a><img src={squarepicicon}/></a></li>
+          <li><a><img src={gificon}/></a></li>
+          <li><a><img src={alignicon}/></a></li>
+          <li><a><img src={noiconicon}/></a></li>
+          <li><a><img src={noiconicon}/></a></li>
+          <li><a><img src={locationicon}/></a></li>
+        </ul>
+        <button>Tweet</button>
       </div>
       <a id="timeline"class="twitter-timeline" href="https://twitter.com/TheSportsMediaC/lists/1543020161199812608?ref_src=twsrc%5Etfw">A Twitter List by TheSportsMediaC</a> 
       <Helmet>
