636828adde4a645f1d75a0d87b56e364f927ba97
CarlG <carl91y@gmail.com>
2022-07-13 14:28:58 -0700

    created react-redux app

---
+++ REACT/react-redux-demo/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+import { Provider,connect } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './redux/reducers'
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
