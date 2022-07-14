32aa201db4a6cc119c869075cd01ee99af169a27
CarlG <carl91y@gmail.com>
2022-07-14 13:45:29 -0700

    created  react section of project

--- REACT/react-redux-demo/src/App.js
+++ REACT/react-redux-demo/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import { useState } from "react";
+import { useState,useRef } from "react";
 
 function App() {
   return (
@@ -14,22 +14,31 @@ function App() {
 
 export default App;
 
-
 //REACT
 function Todo(){
   const [messages,setMessages]=useState([])
+  const [input,setInput]=useState("")
+  const inputElement=useRef();
 
-  const updateMessages=(event)=>{
-    setMessages(prev=>{
-      return [...messages,event.target.value]
-    })
+  const updateMessages=()=>{
+    if (input==""){
+      return alert("You must write something here!")
+    }
+    else{
+      setMessages(prev=>{
+        return [...prev,input]
+        }
+      );
+      setInput("");
+      {/*the setInput is meant to prevent the resubmission of input when clicking the button(empty field) after another click*/}
+    }
   }
-
   return (
     <div>
-        <input onChange={updateMessages}/>
+        <input onChange={(event)=>setInput(event.target.value)} onBlur={(event)=>{event.target.value=""}}/> {/*the onblur is meant to prevent the input text from remaining after button click */}
         <button onClick={updateMessages}>Submit</button>
         <ul>{messages.map(x=>{return (<li>{x}</li>)})}</ul>
     </div>
   )
 }
+
