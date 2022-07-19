f85a9c234ddd943b8b0fb681ce6365a626886899
CarlG <carl91y@gmail.com>
2022-07-19 16:07:50 -0700

    improved quote-box positioning and sizing,changed behavior so that quote shows up on pageload..installed fa thru npm but it's not working for now

--- My_Projects/random-quote-gen/src/redux/components/presentational.js
+++ My_Projects/random-quote-gen/src/redux/components/presentational.js
@@ -1,33 +1,44 @@
 import quotes from "./quotes.js"
-import {useRef} from 'react'
+import {useEffect, useRef} from 'react'
 import {connect} from "react-redux"
 import { mapDispatchToProps, mapStateToProps } from "../mappings.js"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 
 
 function Presentational(props){
+    useEffect(()=>{//function works,but would be better if we could just use handleClick fn..work on it
+        let initIdx=Math.floor(Math.random()*(6.9-0)+0);
+        //props.dispatchId();
+        document.getElementById("text").innerHTML=quotes[initIdx].quoteText;
+        document.getElementById("author").innerHTML="- "+quotes[initIdx].quoteAuthor;
+    },[]);
     const quoteBox=useRef(null);
     const handleClick=()=>{
-        props.dispatchId()
-        document.getElementById("quote-box").innerHTML=quotes[props.indexState].quoteText;
-        document.getElementById("quote-author").innerHTML=quotes[props.indexState].quoteAuthor;
+        props.dispatchId();
+        document.getElementById("text").innerHTML=quotes[props.indexState].quoteText;
+        document.getElementById("author").innerHTML="- "+quotes[props.indexState].quoteAuthor;
     }
     return(
-        <div>
-            <wrapper id="quote-box" ref={quoteBox}>
+        <div id="wrapper">
+            <div id="quote-box" ref={quoteBox}>
                 <div id="text">
                     
                 </div>
                 <div id="author">
 
                 </div>
-                <button id="new-quote" onClick={handleClick}>
-                    New Quote
-                </button>
-                <a id="tweet-quote">
-
-                </a>
-            </wrapper>
+                <div id="footer-row">
+                    <a id="tweet-quote">
+                        <button className="butto">
+                            <FontAwesomeIcon icon="coffee"/> click
+                        </button>
+                    </a>
+                    <button id="new-quote" onClick={handleClick}>
+                        New Quote
+                    </button>      
+                </div>
+            </div>
         </div>
     )
 }
