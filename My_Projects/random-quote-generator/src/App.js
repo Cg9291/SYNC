c0b668e8ded591b9cea9e3ee0a1be7b9aa985ede
CG <105956982+Cg9291@users.noreply.github.com>
2022-07-21 23:08:41 -0700

    removed logo from app

---
+++ My_Projects/random-quote-generator/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import Presentational from './redux/components/presentational.js';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Presentational/>
+    </div>
+  );
+}
+
+ 
---
+++ My_Projects/random-quote-generator/src/redux/components/indexPicker.js
@@ -0,0 +1,6 @@
+let indexPicker=()=>{
+    return Math.floor(Math.random()*(6.9-0)+0)
+}
+
+//quotes quantity=7
+export default indexPicker
