d19717812dad304585c28614e47f475a93aa856a
CarlG <carl91y@gmail.com>
2022-07-28 15:19:12 -0700

    worked on the added all buttons+styling to the project

--- My_Projects/Drum-machine/src/redux/components/Presentational.js
+++ My_Projects/Drum-machine/src/redux/components/Presentational.js
@@ -1,30 +1,14 @@
-import buttons from "./buttons";
-import { useEffect,useRef } from "react";
+import Buttons from "./Buttons.js";
+import React, { useEffect,useRef } from "react";
 
 export default function Presentational(props){
     const btnsRef=useRef();
-
-    useEffect(()=>{
-        btnsRef.current=buttons('jack');
-    })
-    
     return (
         <div>
             <div id="drum-machine">
                 <h1>Presentational</h1>
                 <div id="display"></div>
-                <button className="drum-pad">
-                    Q
-                    <audio src="" className="clip" id="//change">
-                        <source src="" type="">  
-                        </source>
-                    </audio>
-                </button>
-                <div ref={btnsRef}>
-                {buttons('jack')}
-                {buttons('tom')}
-                {buttons('ernesto')}
-                </div>
+                <Buttons/>
             </div>
         </div>
     )
--- My_Projects/Drum-machine/src/redux/components/buttons.js
+++ My_Projects/Drum-machine/src/redux/components/buttons.js
@@ -1,11 +1,82 @@
-export default function buttons(btnID,src,id,){
+import React from "react";
+import '../../buttons.scss'
+
+export default function Buttons(){
     return(
-        <button className="drum-pad">
-                    {btnID}
-                    <audio src="" className="clip" id={"btnID"}>
+        <div className="container-fluid d-flex justify-content-center" >
+            <div className="row-cols-3 justify-content-center text-center" style={{border:'solid blue'}}> 
+                <button className="drum-pad col btn btn-dark">
+                    Q
+                    <audio src="" className="clip" id="Q">
                         <source src="" type="">  
                         </source>
                     </audio>
-        </button>
-    )
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    W
+                    <audio src="" className="clip" id='W'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    E
+                    <audio src="" className="clip" id='E'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    A
+                    <audio src="" className="clip" id='A'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col-4 btn btn-dark">
+                    S
+                    <audio src="" className="clip" id='S'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    D
+                    <audio src="" className="clip" id='D'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    Z
+                    <audio src="" className="clip" id='Z'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    X
+                    <audio src="" className="clip" id='X'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+
+                <button className="drum-pad col btn btn-dark">
+                    C
+                    <audio src="" className="clip" id='C'>
+                        <source src="" type="">  
+                        </source>
+                    </audio>
+                </button>
+            </div>
+        </div>
+        )  
 }
