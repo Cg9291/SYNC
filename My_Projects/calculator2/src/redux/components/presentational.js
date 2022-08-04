a4257f3d8c0933a3953ecbdac902ae7832f722fb
CarlG <carl91y@gmail.com>
2022-08-04 15:46:44 -0700

    worked on calculator logic/initially got it to work, but issues arised when separating calc state and display state

--- My_Projects/calculator2/src/redux/components/presentational.js
+++ My_Projects/calculator2/src/redux/components/presentational.js
@@ -6,73 +6,104 @@ import { useState } from "react";
 
 export default function Presentational(){
     const [input,setInput]=useState([0]);
-    const [calc,setCalc]=useState();
+    const [inputLength,setInputLength]=useState(0);//index
+    const [operatorsList,setOperatorsList]=useState(['+','-','x','/']);
+    const [calc,setCalc]=useState([]);
     const [operator,setOperator]=useState();
     const [output,setOutput]=useState();
     const [lastIsOperator,setLastIsOperator]=useState(false);
 
-    let turnToOperators={
-        'x':function(a,b){return a*b},
-        '+':function(a,b){return a+b},
-        '/'(a,b){return a/b},
-        '-'(a,b){return a-b}
-    };
-    
-    const handleClick=(event)=>{
-        let etg=event.target.value;
-        if(input==0){
-            if(lastIsOperator===false){
-                if(turnToOperators.hasOwnProperty(etg)){
-                    setCalc(input);
-                    setInput(input.concat(etg));
-                    setOperator(turnToOperators[etg]);
-                    setLastIsOperator(true);
-                }
-                else{
-                    setInput([etg]);
-                }
-            }
-            else{
-                setInput(input.concat(etg));
-            }
-        }else{
-            if(lastIsOperator===false){
-                if(turnToOperators.hasOwnProperty(etg)){
-                    setCalc(input);
-                    setInput(input.concat(etg));
-                    setOperator(turnToOperators[etg]);
-                    setLastIsOperator(true);
-                }
-                else{
-                    setInput(input.concat(etg));
-                }
-            }
-            else{
-                setInput(input.concat(etg));
-        }}        
+    let turnToOperators=(op,a,b)=>{  
+        return op=='x'?a*b
+        :op=='+'?a+b
+        :op=='/'?a/b
+        :op=='-'?a-b
+        :'sign not recognized'
     };
 
+    const handleClick=(event)=>{
+    let etg=event.target.value;
+        switch(input.length){
+            case 1:
+                switch(input[0]){
+                    case 0:
+                        setInput([etg]);
+                        break;
+                    default:
+                        setInput(input.concat(etg));
+                };
+                
+            
+        }
+    }
+
+
+    /*const handleClick=(event)=>{
+        let etg=event.target.value;
+        if(input==0){//NO INPUT YET
+            if(lastIsOperator===false){
+                if(operatorsList.indexOf(etg)>=0){//entered an operator
+                    setCalc(input.slice());
+                    setOperator(etg);
+                    setInput(input.concat(etg));
+                    setLastIsOperator(true);
+                }
+                else{
+                    //setCalc('etg='+etg);
+                    setInput([etg]);
+                    setLastIsOperator(false);
+                }
+            }
+            else{//should be entering a number
+                setCalc(calc.concat(input[1]));
+                setInput(input.concat(etg));
+                setLastIsOperator(false);
+            }
+        }else{
+            if(lastIsOperator===false){//INPUT ALREADY EXISTS
+                if(operatorsList.indexOf(etg)>=0){//entered an operator
+                    setCalc(input.slice);
+                    setOperator(etg);
+                    setInput(input.concat(etg));
+                    setLastIsOperator(true);
+                }
+                else{
+                    //setCalc('etg='+etg);
+                    setInput(input.concat(etg));
+                    setLastIsOperator(false);
+                }
+            }
+            else{
+                setCalc(calc.concat(input[1]));
+                setInput(input.concat(etg));
+                setLastIsOperator(false);
+        }}        
+    };*/
+
     const handleEqualClick=()=>{
-     setOutput(operator(1,2));
+     setOutput(turnToOperators(operator,Number(calc[0]),Number(calc[2])))
     };
 
     const handleAcClick=()=>{
         setInput([]);
+        setCalc([]);
+        setOutput([]);
+        setOperator();
     }
 
     return(
         <div>
             <div className="container col-3 px-1 pb-1 pt-4 justify-content-center justify-self-center bg-dark border border-primary">
                 <div id="display" className="row-cols-4 justify-content text-light" onClick={handleClick}>
-                   {input} //{output}
+                   {input}/{operator}/{output}
                 </div>
                 <div className="row-cols-4 justify-content">
                     <button id='clear' className="col-6 btn btn-danger shadow-none rounded-0 border" value={'AC'} onClick={handleAcClick}>AC</button>
                     <button id="divide" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'/'} onClick={handleClick}>/</button>
-                    <button id="multiply" className="col-3 btn btn-primary shadow-none rounded-0 border" value='x' onClick={handleClick}>x</button>
+                    <button id="multiply" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'x'} onClick={handleClick}>x</button>
                 </div>
                 <div className="row-cols-4  justify-content">
-                    <button id="seven" className="col-3 btn btn-dark shadow-none rounded-0 border" value={7} onClick={handleClick}>7</button>
+                    <button id="seven" className="col-3 btn btn-dark shadow-none rounded-0 border" value='7' onClick={handleClick}>7</button>
                     <button id="eight" className="col-3 btn btn-dark shadow-none rounded-0 border" value={8} onClick={handleClick}>8</button>
                     <button id="nine" className="col-3 btn btn-dark shadow-none rounded-0 border" value={9} onClick={handleClick}>9</button>
                     <button id="subtract" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'-'} onClick={handleClick}>-</button>
