95977fc091a9725b6338e60af357f19804c34865
CarlG <carl91y@gmail.com>
2022-08-05 16:31:25 -0700

    fixed calc logic for single digits

--- My_Projects/calculator2/src/redux/components/presentational.js
+++ My_Projects/calculator2/src/redux/components/presentational.js
@@ -1,17 +1,33 @@
 import {connect } from "react-redux"
-import { useState } from "react";
+import { useEffect, useState,useRef } from "react";
 //import { mapStateToProps,mapDispatchToProps } from "react-redux"
 //import { mapStateToProps } from "../../../../random-quote-gen/src/redux/mappings"
 //FIGURE OUT HOW TO SELECT ALL ELEMENTS OF A CLASS/TYPE AND APPLY A FN/EVENTHANDLER TO THEM(REACT WAY)
 
 export default function Presentational(){
-    const [input,setInput]=useState([0]);
-    const [inputLength,setInputLength]=useState(0);//index
-    const [operatorsList,setOperatorsList]=useState(['+','-','x','/']);
-    const [calc,setCalc]=useState([]);
+    const [input,setInput]=useState([0]);  
+    const [lastIndex,setLastIndex]=useState(false);//index
+    const [operatorsList,setOperatorsList]=useState(['+','-','x','/']);  
     const [operator,setOperator]=useState();
-    const [output,setOutput]=useState();
-    const [lastIsOperator,setLastIsOperator]=useState(false);
+    const [tally,setTally]=useState(0);
+    const [secondVar,setSecondVar]=useState();
+    const [result,setResult]=useState(0);
+    const [clickStatus,setClickStatus]=useState();
+    //const [isOperator,setLastIsOperator]=useState();
+    // const [output,setOutput]=useState();
+    const [displayStyle,setDisplayStyle]=useState({backgroundColor:'red'});
+
+    const btnRef=useRef();
+
+    function handleKeyPress(event){
+        setClickStatus(event.key);
+        if(clickStatus==btnRef.current.value){
+            btnRef.current.style=displayStyle;
+        }
+        
+    };
+
+    useEffect(()=>window.addEventListener("keydown",handleKeyPress,false),[]);
 
     let turnToOperators=(op,a,b)=>{  
         return op=='x'?a*b
@@ -22,20 +38,80 @@ export default function Presentational(){
     };
 
     const handleClick=(event)=>{
+        let etv=event.target.value;
+        let isOperator=(val)=>{
+            return operatorsList.indexOf(val)>=0;
+        };
+        
+        if(input[0]===0){
+            if(isOperator(etv)){//if click on operator @0
+                setOperator(etv);
+                setInput([etv]);
+                setLastIndex(true);
+                //setResult(etv)
+            }
+            else{//if click on number @0
+                setTally(etv);
+                setInput([etv]);
+                setLastIndex(false);
+            }
+        }
+        else if(lastIndex===true){//if last entered is operator
+            if(isOperator(etv)===false){//if click on number @operator
+                setSecondVar(etv);
+                setInput([...input,etv]);
+                setLastIndex(false);
+                }
+            }
+        else if (lastIndex===false){//if last entered is number
+            if(isOperator(etv)===true){//if click on operator @number
+                setOperator(etv);
+                setInput([...input,etv]);
+                setLastIndex(true);
+                }
+            else if(isOperator(etv)===false){//if click on number @number
+                setInput([...input,etv]);
+                setLastIndex(false);
+
+                }
+        }    
+        }
+
+
+    
+
+    /*const handleClick=(event)=>{
     let etg=event.target.value;
-        switch(input.length){
-            case 1:
-                switch(input[0]){
-                    case 0:
-                        setInput([etg]);
+    let isOperator=(val)=>{
+        return operatorsList.indexOf(val)>=0;
+    }
+        switch(true){
+            case input[0]==0:
+                setInput([etg]);
+                break;
+            case isOperator(input[0]):
+                switch(true){
+                    case isOperator(etg):
+                        setOperator(true);
+                        //setDisplayStyle({backgroundColor:'red'});
                         break;
                     default:
-                        setInput(input.concat(etg));
+                        setInput([...input,etg]);
+                        break;   
+                }
+                break;
+            default:
+                switch(true){
+                    case isOperator(etg):
+                        setOperator(true);
+                        setInput([...input,etg])
+                        break;
+                    default:
+                        setInput([...input,etg]);
+                        break; 
+                }
                 };
-                
-            
-        }
-    }
+    }*/
 
 
     /*const handleClick=(event)=>{
@@ -81,21 +157,22 @@ export default function Presentational(){
     };*/
 
     const handleEqualClick=()=>{
-     setOutput(turnToOperators(operator,Number(calc[0]),Number(calc[2])))
+     setTally(turnToOperators(operator,Number(tally),Number(secondVar)));
+     return tally;
     };
 
     const handleAcClick=()=>{
-        setInput([]);
-        setCalc([]);
-        setOutput([]);
+        setInput([0]);
+        setTally(0)
+        //setOutput([]);
         setOperator();
     }
 
     return(
         <div>
             <div className="container col-3 px-1 pb-1 pt-4 justify-content-center justify-self-center bg-dark border border-primary">
-                <div id="display" className="row-cols-4 justify-content text-light" onClick={handleClick}>
-                   {input}/{operator}/{output}
+                <div id="display" /*style={displayStyle}*/ className="row-cols-4 justify-content text-light" onClick={handleClick}>
+                   {input}={tally}//{clickStatus}
                 </div>
                 <div className="row-cols-4 justify-content">
                     <button id='clear' className="col-6 btn btn-danger shadow-none rounded-0 border" value={'AC'} onClick={handleAcClick}>AC</button>
@@ -103,9 +180,9 @@ export default function Presentational(){
                     <button id="multiply" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'x'} onClick={handleClick}>x</button>
                 </div>
                 <div className="row-cols-4  justify-content">
-                    <button id="seven" className="col-3 btn btn-dark shadow-none rounded-0 border" value='7' onClick={handleClick}>7</button>
-                    <button id="eight" className="col-3 btn btn-dark shadow-none rounded-0 border" value={8} onClick={handleClick}>8</button>
-                    <button id="nine" className="col-3 btn btn-dark shadow-none rounded-0 border" value={9} onClick={handleClick}>9</button>
+                    <button id="seven" ref={btnRef} className="col-3 btn btn-dark shadow-none rounded-0 border" value={7} onClick={handleClick}>7</button>
+                    <button id="eight" ref={btnRef} className="col-3 btn btn-dark shadow-none rounded-0 border" value={8} onClick={handleClick}>8</button>
+                    <button id="nine" ref={btnRef} className="col-3 btn btn-dark shadow-none rounded-0 border" value={9} onClick={handleClick} onKeyPress={handleKeyPress}>9</button>
                     <button id="subtract" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'-'} onClick={handleClick}>-</button>
                 </div>
                 <div className="row-cols-4 justify-content">
