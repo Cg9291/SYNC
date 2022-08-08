8e120614da80f35929f66a79fbec1aba9eccec14
CarlG <carl91y@gmail.com>
2022-08-08 16:18:12 -0700

    calculation tweaks

--- My_Projects/calculator2/src/redux/components/presentational.js
+++ My_Projects/calculator2/src/redux/components/presentational.js
@@ -5,12 +5,13 @@ import { useEffect, useState,useRef } from "react";
 //FIGURE OUT HOW TO SELECT ALL ELEMENTS OF A CLASS/TYPE AND APPLY A FN/EVENTHANDLER TO THEM(REACT WAY)
 
 export default function Presentational(){
-    const [input,setInput]=useState([0]);  
+    const [input,setInput]=useState([]);  
+    const [output,setOutput]=useState(0)
     const [lastIndex,setLastIndex]=useState(false);//index
     const [operatorsList,setOperatorsList]=useState(['+','-','x','/']);  
     const [operator,setOperator]=useState();
     const [tally,setTally]=useState(0);
-    const [secondVar,setSecondVar]=useState();
+    const [secondVar,setSecondVar]=useState([]);
     const [result,setResult]=useState(0);
     const [clickStatus,setClickStatus]=useState();
     //const [isOperator,setLastIsOperator]=useState();
@@ -39,43 +40,40 @@ export default function Presentational(){
 
     const handleClick=(event)=>{
         let etv=event.target.value;
-        let isOperator=(val)=>{
-            return operatorsList.indexOf(val)>=0;
-        };
-        
-        if(input[0]===0){
-            if(isOperator(etv)){//if click on operator @0
-                setOperator(etv);
-                setInput([etv]);
-                setLastIndex(true);
-                //setResult(etv)
-            }
-            else{//if click on number @0
-                setTally(etv);
-                setInput([etv]);
-                setLastIndex(false);
-            }
+    
+        if(output===0){
+            setOutput(etv);
+            setInput([...input,etv])
         }
-        else if(lastIndex===true){//if last entered is operator
-            if(isOperator(etv)===false){//if click on number @operator
-                setSecondVar(etv);
-                setInput([...input,etv]);
-                setLastIndex(false);
-                }
-            }
-        else if (lastIndex===false){//if last entered is number
-            if(isOperator(etv)===true){//if click on operator @number
-                setOperator(etv);
-                setInput([...input,etv]);
-                setLastIndex(true);
-                }
-            else if(isOperator(etv)===false){//if click on number @number
-                setInput([...input,etv]);
-                setLastIndex(false);
+        else if (output>0){
+            setOutput(output+etv);
+            setInput([...input.concat(etv)])
+        }
+        else{
+            setSecondVar([...secondVar,etv]);
+            setOutput();
+            setInput([...input.concat(etv)])
+        }
+    }
 
-                }
-        }    
+    const handleClickOperator=(event)=>{
+        let etv=event.target.value;
+        if(output===0){
+            setOperator(etv);
+            setOutput(etv);
+            setInput([...input,etv])
         }
+        else if(output>0){
+            setTally(tally+Number(output));
+            setOperator(etv);
+            setOutput(etv);
+            setInput([...input,etv])
+        }
+        
+
+    }
+        
+        
 
 
     
@@ -158,13 +156,13 @@ export default function Presentational(){
 
     const handleEqualClick=()=>{
      setTally(turnToOperators(operator,Number(tally),Number(secondVar)));
-     return tally;
+     
     };
 
     const handleAcClick=()=>{
-        setInput([0]);
+        setInput([]);
         setTally(0)
-        //setOutput([]);
+        setOutput(0);
         setOperator();
     }
 
@@ -172,29 +170,29 @@ export default function Presentational(){
         <div>
             <div className="container col-3 px-1 pb-1 pt-4 justify-content-center justify-self-center bg-dark border border-primary">
                 <div id="display" /*style={displayStyle}*/ className="row-cols-4 justify-content text-light" onClick={handleClick}>
-                   {input}={tally}//{clickStatus}
+                   {output}/{input}/{tally}
                 </div>
                 <div className="row-cols-4 justify-content">
                     <button id='clear' className="col-6 btn btn-danger shadow-none rounded-0 border" value={'AC'} onClick={handleAcClick}>AC</button>
-                    <button id="divide" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'/'} onClick={handleClick}>/</button>
-                    <button id="multiply" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'x'} onClick={handleClick}>x</button>
+                    <button id="divide" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'/'} onClick={handleClickOperator}>/</button>
+                    <button id="multiply" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'x'} onClick={handleClickOperator}>x</button>
                 </div>
                 <div className="row-cols-4  justify-content">
                     <button id="seven" ref={btnRef} className="col-3 btn btn-dark shadow-none rounded-0 border" value={7} onClick={handleClick}>7</button>
                     <button id="eight" ref={btnRef} className="col-3 btn btn-dark shadow-none rounded-0 border" value={8} onClick={handleClick}>8</button>
                     <button id="nine" ref={btnRef} className="col-3 btn btn-dark shadow-none rounded-0 border" value={9} onClick={handleClick} onKeyPress={handleKeyPress}>9</button>
-                    <button id="subtract" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'-'} onClick={handleClick}>-</button>
+                    <button id="subtract" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'-'} onClick={handleClickOperator}>-</button>
                 </div>
                 <div className="row-cols-4 justify-content">
-                    <button id="4" className="col-3 btn btn-dark shadow-none rounded-0 border" value={4} onClick={handleClick}>4</button>
-                    <button id="5" className="col-3 btn btn-dark shadow-none rounded-0 border" value={5} onClick={handleClick}>5</button>
-                    <button id="6" className="col-3 btn btn-dark shadow-none rounded-0 border" value={6} onClick={handleClick}>6</button>
-                    <button id="add" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'+'} onClick={handleClick}>+</button>
+                    <button id="four" className="col-3 btn btn-dark shadow-none rounded-0 border" value={4} onClick={handleClick}>4</button>
+                    <button id="five" className="col-3 btn btn-dark shadow-none rounded-0 border" value={5} onClick={handleClick}>5</button>
+                    <button id="six" className="col-3 btn btn-dark shadow-none rounded-0 border" value={6} onClick={handleClick}>6</button>
+                    <button id="add" className="col-3 btn btn-primary shadow-none rounded-0 border" value={'+'} onClick={handleClickOperator}>+</button>
                 </div>
                 <div className="row-cols-4 justify-content">
-                    <button id="1" className="col-3 btn btn-dark shadow-none rounded-0 border" value={1} onClick={handleClick}>1</button>
-                    <button id="2" className="col-3 btn btn-dark shadow-none rounded-0 border" value={2} onClick={handleClick}>2</button>
-                    <button id="3" className="col-3 btn btn-dark shadow-none rounded-0 border" value={3} onClick={handleClick}>3</button>
+                    <button id="one" className="col-3 btn btn-dark shadow-none rounded-0 border" value={1} onClick={handleClick}>1</button>
+                    <button id="two" className="col-3 btn btn-dark shadow-none rounded-0 border" value={2} onClick={handleClick}>2</button>
+                    <button id="three" className="col-3 btn btn-dark shadow-none rounded-0 border" value={3} onClick={handleClick}>3</button>
                     <button id="equals" className="col-3 btn btn-success shadow-none rounded-0 border" value={'='} onClick={handleEqualClick}>=</button>
                 </div>
                 <div className="row-cols-4 justify-content">
@@ -207,4 +205,4 @@ export default function Presentational(){
     )
 }
 
-//export default connect(mapStateToProps,mapDispatchToProps)(Presentational)
+//export default connect(mapStateToProps,mapDispatchToProps)(Presentational
