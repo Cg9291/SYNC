b9b481192bd81ef15b6c1cd9a1201db95112138f
CG <105956982+Cg9291@users.noreply.github.com>
2022-08-14 23:24:40 -0700

    formatted code

--- My_Projects/calculator2/src/redux/components/Display.js
+++ My_Projects/calculator2/src/redux/components/Display.js
@@ -11,7 +11,7 @@ export default function Display(props){
             <div>
               <div /*style={displayStyle}*/ className="row-cols-4 justify-content text-light d-flex flex-column        align-items-center" onClick={props.handleClick}>
               </div>  
-            <div id="display" /*style={displayStyle}*/ className="row-cols-4 justify-content text-light d-flex flex-column align-items-center" onClick={props.handleClick}>
+                <div id="display" /*style={displayStyle}*/ className="row-cols-4 justify-content text-light d-flex flex-column align-items-center" onClick={props.handleClick}>
               <div>{props.outputState}</div>
               {/*<div>{props.inputState}</div>*/}
             </div>
