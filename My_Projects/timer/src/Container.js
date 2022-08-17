4cd199a7cb58a98b28fb32ee4320628c85f57ba8
CarlG <carl91y@gmail.com>
2022-08-17 16:25:47 -0700

    started implementing timer logic

--- My_Projects/timer/src/Container.js
+++ My_Projects/timer/src/Container.js
@@ -1,21 +1,52 @@
 import Break from "./components/BreakComponent";
 import Session from "./components/SessionComponent";
 import Timer from "./components/TimerComponent";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 export default function Container(){
     const [timeElapsed,setTimeElapsed]=useState(0);
-    const [seconds,setSeconds]=useState(new Date(20/20/20))
+    const [trackMinutes,setTrackMinutes]=useState(25);
+    const [trackSeconds,setTrackSeconds]=useState(0);
+    const [seconds,setSeconds]=useState(new Date(0,0,0,0,0,trackSeconds).getSeconds());
+    const [minutes,setMinutes]=useState(new Date (0,0,0,0,trackMinutes,0).getMinutes());
+    const [startStop,setStartStop]=useState(false);
+
+
+    /*useEffect(()=>{
+        //document.addEventListener("click",startTimer())
+        setInterval(() => {
+            setSeconds(seconds+1)
+         },1000)
+    })*/
+   
+    var myTime=new Date(0,0,0,0,minutes,seconds)
+
     let startTimer=()=>{
         setInterval(() => {
-           setTimeElapsed(timeElapsed+1)
-        },1000);
+            if(seconds===0){
+                setTrackMinutes((trackMinutes)=>trackMinutes-1)
+                setTrackSeconds((trackSeconds)=>trackSeconds-1)
+            }
+            else if (seconds>0){
+                setTrackSeconds((trackSeconds)=>trackSeconds-1)
+         }},1000);
+       /* if(startStop){
+            setStartStop(false);
+        }
+        else{
+            setStartStop(true);
+        };*/
+        
+        
     };
 
     Timer.defaultProps={
-        timeState:timeElapsed,
+        timeElpasedState:timeElapsed,
         timeFunction:startTimer,
-        secondsState:seconds
+        secondsState:seconds,
+        minutesState:minutes,
+        trackMinutesState:trackMinutes,
+        trackSecondsState:trackSeconds
     }
 
     return(
@@ -26,3 +57,4 @@ export default function Container(){
     </div>
     );
 }
+
