7768c12821139cba54a63e77a8332167c76f6d7e
CarlG <carl91y@gmail.com>
2022-08-23 16:11:59 -0700

    worked on FCCnode tutorial

--- myApp.js
+++ myApp.js
@@ -1,11 +1,21 @@
 //MAKE SURE TO NEVER PUSH ENV FILE TO GITHUB, ADD IT TO GIT IGNORE LIST
+require('dotenv').config({path:'SYNC/JAVASCRIPT_CODE/NODEJS/Node FCC Tutorial/boilerplate-express/.env'});
+//FCC Q# [Use the .env File]=>Biggest hurdle here was:
+//1-not being able to access env vars unless in the root(programming folder//non project root)
+//2- not being able to use comparison operator even able to access env vars through non project root
+//3- had to read documentation to be able to add path in config up there
 var livereload = require("livereload");
 var connectLiveReload = require("connect-livereload");
 
 let express = require('express');
+const { json } = require('body-parser');
 let app = express();
-require('dotenv').config();
 
+app.use("/",(req,res,next)=>{//could not submit,works it terminal but not when submitted on fcc, found multiple posts pointing to error on their end..will follow up
+    let string=`${req.method} ${req.path} - ${req.ip}`;
+    console.log(string);
+    next();
+});
 
 
 app.get("/",(req,res)=>{
@@ -15,14 +25,24 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/json",(req,res)=>{  
-    if(JSON.stringify(process.env.MESSAGE_STYLE)===JSON.stringify('uppercase')){//not working
-        res.json({"messages": "HELLO JSON"})
-    }else{
+    if(process.env.MESSAGE_STYLE=="uppercase"){
+        res.json({"message": "HELLO JSON"})
+    }
+    else{
         res.json({"message": "Hello json"})
     }
 })
 
-console.log(process.env.MESSAGE_STYLE)
+app.get("/now",(req,res,next)=>{
+    req.time=new Date().toString();
+    next();
+},
+    function(req,res){
+        res.json({time:req.time});
+    }
+)
+
+
 
 
 
