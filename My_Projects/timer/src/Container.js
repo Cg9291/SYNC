9c09cc1c36d81cbd9bd4637348bbffb1bb3ed05b
CarlG <carl91y@gmail.com>
2022-08-26 16:18:44 -0700

    worked on timer

--- My_Projects/timer/src/Container.js
+++ My_Projects/timer/src/Container.js
@@ -1,20 +1,21 @@
 import Break from "./components/BreakComponent";
 import Session from "./components/SessionComponent";
 import Timer from "./components/TimerComponent";
-import { useState,useEffect,useRef } from "react";
+import { useState,useEffect,useLayoutEffect,useRef } from "react";
 
 export default function Container(){
    
     //break component hooks
     const [breakLength,setBreakLength]=useState(5);
     //session component hooks
-    const [sessionLength,setSessionLength]=useState(0)
+    const [sessionLength,setSessionLength]=useState(25)
     
     //new date object/state
-    const [time,setTime]=useState()
-    const [timeMinutes,setTimeMinutes]=useState(sessionLength);
-    const [timeSeconds,setTimeSeconds]=useState(3)
-
+    const [time,setTime]=useState(new Date(0,0,0,0,25,0))
+    const [timeSeconds,setTimeSeconds]=useState(0)
+    let minutes=time.getMinutes();
+    //let secondsRef=useRef(time.getSeconds())
+    let seconds=time.getSeconds()
     //timer start stop state
     const [started,setStarted]=useState(false)
     const intvl = useRef();//THIS CAME CLUTCH IN MAKING THE TIMER WORK AS INTENTED(STOP AT 00:00)
@@ -30,34 +31,40 @@ export default function Container(){
     }
 
     let sessionHandler=(event)=>{
+        if(started!==true){
         if(event.target.id=="session-increment"){
             setSessionLength((sessionLength)=>sessionLength+1);
         }
         else if(event.target.id=="session-decrement" && sessionLength>1){
             setSessionLength((sessionLength)=>sessionLength-1)
         }
-    }
+    }}
 
     let breakHandler=(event)=>{
+        if(started!==true){
         if(event.target.id=="break-increment"){
             setBreakLength((breakLength)=>breakLength+1);
         }
         else if(event.target.id=="break-decrement" && breakLength>1){
             setBreakLength((breakLength)=>breakLength-1);
         }
-    }
+    }}
 
     let refreshHandler=()=>{
         setBreakLength(5);
         setSessionLength(25);
-        setTime(new Date(0,0,0,0,sessionLength,timeSeconds).toLocaleTimeString());
-        setTimeMinutes(0);
-        setTimeSeconds(0)
+        setTime(new Date(0,0,0,0,25,0));
+        setTimeSeconds(0);
+        setStarted(false);
     }
 
-    useEffect(()=>setTime(new Date(0,0,0,0,sessionLength,timeSeconds).toLocaleTimeString()),[timeSeconds,sessionLength])
+    if(time.getSeconds()<10){
+        seconds='0'+seconds
+    }
+    useEffect(()=>setTime(new Date(0,0,0,0,sessionLength,timeSeconds)),[timeSeconds,sessionLength])
 
     useEffect(()=>{
+        document.title="Cg's Timer Project"
         //document.addEventListener("click",startTimer())
         if(started===true){
              intvl.current=setInterval(() => {
@@ -67,10 +74,22 @@ export default function Container(){
         }
     },[started,sessionLength,timeSeconds])
 
+    useEffect(()=>{
+
+            seconds='00';
+        
+    },[timeSeconds])
+
     useEffect(()=>{if(sessionLength==0 && timeSeconds==0){
         clearInterval(intvl.current);
         setStarted(false)
     }} )
+
+   /* useEffect(()=>{
+        if(secondsRef.current<10){
+            seconds='0'+secondsRef.current
+        }
+    },[secondsRef.current])*/
     
     
 
@@ -83,16 +102,20 @@ export default function Container(){
         startedState:started,
         startTimerFunction:startTimer,
         refreshHandler:refreshHandler,
+        minutesState:minutes,
+        secondsState:seconds,
     }
 
     Break.defaultProps={
         breakLengthState:breakLength,
-        breakHandler:breakHandler
+        breakHandler:breakHandler,
+        startedState:started
     }
 
     Session.defaultProps={
         sessionLengthState:sessionLength,
-        sessionHandler:sessionHandler
+        sessionHandler:sessionHandler,
+        startedState:started
     }
 
     return(
--- My_Projects/timer/src/components/TimerComponent.js
+++ My_Projects/timer/src/components/TimerComponent.js
@@ -5,8 +5,8 @@ export default function Timer(props){
         <div>
             <div className="fa fa-car" id="timer-label">Time remaining</div>
 
-            {props.startedState===true && <div id="time-left" style={{color:'red'}}>{props.sessionLengthState}:{props.timeSecondsState}//{props.timeState}</div>} 
-            {props.startedState===false && <div id="time-left">{props.sessionLengthState}:{props.timeSecondsState}//{props.timeState}</div>}
+            {props.startedState===true && <div id="time-left" style={{color:'red'}}>{props.minutesState}:{props.secondsState}</div>} 
+            {props.startedState===false && <div id="time-left">{props.minutesState}:{props.secondsState}</div>}
 
             {props.startedState===true && <button id="start_stop" className="fa fa-pause" style={{color:'red'}} onClick={props.startTimerFunction}></button>}
             {props.startedState===false && <button id="start_stop" className="fa fa-play" onClick={props.startTimerFunction}></button>}
