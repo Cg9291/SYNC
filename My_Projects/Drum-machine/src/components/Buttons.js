39ae1bfda6d27c47300a4be62fbc61a16e1aa98e
CarlG <carl91y@gmail.com>
2022-09-09 16:01:04 -0700

    added new audio files and changed ID names...NOW PASSING ALL 8 TESTS!

--- My_Projects/Drum-machine/src/components/Buttons.js
+++ My_Projects/Drum-machine/src/components/Buttons.js
@@ -1,8 +1,16 @@
+//MUST REPLACE KICK!!! (FILE DOESNT WORK)
+//OPTIONAL IMPROVEMENTS:(1-CHANGE KEYS COLOR WHEN PRESSED/CLICKED 2-ADD WINDOWS FOR LABEL AND POWER ON/OFF 3-FIX UNEVEN VOLUME)
 import React, { useEffect, useRef, useState } from "react";
 import '../buttons.scss';
-import beepAudio from "../audio/beep.mp3";
-import iosNotificationAudio from "../audio/ios_notification.mp3";
-import iosMessageAudio from "../audio/ios_message_sent.mp3"
+import clap_Sound from '../audio/clap.mp3';
+import closed_HH_Sound from '../audio/closed-HH.mp3';
+import heater_1_Sound from '../audio/heater-1.mp3';
+import heater_2_Sound from '../audio/heater-2.mp3';
+import heater_3_Sound from '../audio/heater-3.mp3';
+import heater_4_Sound from '../audio/heater-4.mp3';
+import kick_n_hat_Sound from '../audio/kick_n_Hat.mp3';
+import kick_Sound from '../audio/kick.mp3';
+import open_HH_Sound from '../audio/open - HH.mp3';
 
 export default function Buttons(){
 
@@ -63,66 +71,66 @@ export default function Buttons(){
             <div id="display">{playing}</div>
             <div className="row justify-content-center">
             <div id="pad" className="col-4 row-cols-3" style={{border:'solid blue'}}> 
-                <button id="Q1" className="drum-pad col btn btn-dark"  onClick={playSound}>
+                <button id="Clap" className="drum-pad col btn btn-dark" onClick={playSound}>
                     Q
-                    <audio src={beepAudio} className="clip" id="Q" ref={Q}>
-                       
+                    <audio src={clap_Sound} className="clip" id="Q" ref={Q}>
+                        Your browser does not support this type of file.  
                     </audio>
                 </button>
 
-                <button id="W1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Closed HH" className="drum-pad col btn btn-dark" onClick={playSound}>
                     W
-                    <audio src={iosNotificationAudio} className="clip" id='W' ref={W}>
-                        
+                    <audio src={closed_HH_Sound} className="clip" id='W' ref={W}>
+                        Your browser does not support this type of file.   
                     </audio>
                 </button>
 
-                <button id="E1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Heater 1" className="drum-pad col btn btn-dark" onClick={playSound}>
                     E
-                    <audio src={iosMessageAudio} className="clip" id='E' ref={E}>
-                        
+                    <audio src={heater_1_Sound} className="clip" id='E' ref={E}>
+                        Your browser does not support this type of file.     
                     </audio>
                 </button>
 
-                <button id="A1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Heater 2" className="drum-pad col btn btn-dark" onClick={playSound}>
                     A
-                    <audio src={iosNotificationAudio} className="clip" id='A' ref={A}>
-                        
+                    <audio src={heater_2_Sound} className="clip" id='A' ref={A}>
+                        Your browser does not support this type of file.  
                     </audio>
                 </button>
 
-                <button id="S1" className="drum-pad col-4 btn btn-dark" onClick={playSound}>
+                <button id="Heater 3" className="drum-pad col-4 btn btn-dark" onClick={playSound}>
                     S
-                    <audio src={iosNotificationAudio} className="clip" id='S' ref={S}>
-                        
+                    <audio src={heater_3_Sound} className="clip" id='S' ref={S}>
+                        Your browser does not support this type of file.  
                     </audio>
                 </button>
 
-                <button id="D1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Heater 4" className="drum-pad col btn btn-dark" onClick={playSound}>
                     D
-                    <audio src={iosNotificationAudio} className="clip" id='D' ref={D}>
-                        
+                    <audio src={heater_4_Sound} className="clip" id='D' ref={D}>
+                        Your browser does not support this type of file.  
                     </audio>
                 </button>
 
-                <button id="Z1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Kick n'Hat" className="drum-pad col btn btn-dark" onClick={playSound}>
                     Z
-                    <audio src={iosNotificationAudio} className="clip" id='Z' ref={Z}>
-                        
+                    <audio src={kick_n_hat_Sound} className="clip" id='Z' ref={Z}>
+                        Your browser does not support this type of file.  
                     </audio>
                 </button>
 
-                <button id="X1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Kick" className="drum-pad col btn btn-dark" onClick={playSound}>
                     X
-                    <audio src={iosNotificationAudio} className="clip" id='X' ref={X}>
-                        
+                    <audio src={kick_Sound} className="clip" id='X' ref={X} volume="0.9">
+                        Your browser does not support this type of file.  
                     </audio>
                 </button>
 
-                <button id="C1" className="drum-pad col btn btn-dark" onClick={playSound}>
+                <button id="Open HH" className="drum-pad col btn btn-dark" onClick={playSound}>
                     C
-                    <audio src={iosNotificationAudio} className="clip" id='C' ref={C}>
-                        
+                    <audio src={open_HH_Sound} className="clip" id='C' ref={C}>
+                        Your browser does not support this type of file.   
                     </audio>
                 </button>
                 </div>
