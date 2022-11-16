1500cd459ea8b38365cbf323c11558b9c6bdc36c
CarlG <carl91y@gmail.com>
2022-11-16 13:40:55 -0800

    resized and repostioned pad to allow for right pane

--- Main_Folder/MY_PROJECTS/drum-machine/src/components/Buttons.js
+++ Main_Folder/MY_PROJECTS/drum-machine/src/components/Buttons.js
@@ -67,72 +67,78 @@ export default function Buttons(){
    },[]);
 
     return(
-        <div className="container-fluid justify-content-center align-items-center">
+        <div className="container-fluid justify-content-center align-items-center col-6 m-auto">
             <div id="display">{playing}</div>
-            <div className="row justify-content-center">
-            <div id="pad" className="col-3 " style={{border:'solid blue'}}> 
-                <button id="Clap" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px red"}}>
-                    Q
-                    <audio src={clap_Sound} className="clip" id="Q" ref={Q}>
-                        Your browser does not support this type of file.  
-                    </audio>
-                </button>
+            <div className="row m-auto" style={{border:'solid green'}}>
+                <div id="pad" className="row col-6" style={{border:'solid blue'}}> 
+                    <div className="row justify-content-center">
+                        <button id="Clap" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px red"}}>
+                            Q
+                            <audio src={clap_Sound} className="clip" id="Q" ref={Q}>
+                                Your browser does not support this type of file.  
+                            </audio>
+                        </button>
 
-                <button id="Closed HH" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px blue"}}>
-                    W
-                    <audio src={closed_HH_Sound} className="clip" id='W' ref={W}>
-                        Your browser does not support this type of file.   
-                    </audio>
-                </button>
+                        <button id="Closed HH" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px blue"}}>
+                            W
+                            <audio src={closed_HH_Sound} className="clip" id='W' ref={W}>
+                                Your browser does not support this type of file.   
+                            </audio>
+                        </button>
 
-                <button id="Heater 1" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px green"}}>
-                    E
-                    <audio src={heater_1_Sound} className="clip" id='E' ref={E}>
-                        Your browser does not support this type of file.     
-                    </audio>
-                </button>
+                        <button id="Heater 1" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px green"}}>
+                            E
+                            <audio src={heater_1_Sound} className="clip" id='E' ref={E}>
+                                Your browser does not support this type of file.     
+                            </audio>
+                        </button>
+                        </div>
 
-                <button id="Heater 2" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px purple"}}>
-                    A
-                    <audio src={heater_2_Sound} className="clip" id='A' ref={A}>
-                        Your browser does not support this type of file.  
-                    </audio>
-                </button>
+                    <div className="row justify-content-center">                
+                        <button id="Heater 2" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px purple"}}>
+                            A
+                            <audio src={heater_2_Sound} className="clip" id='A' ref={A}>
+                                Your browser does not support this type of file.  
+                            </audio>
+                        </button>
 
-                <button id="Heater 3" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px magenta"}}>
-                    S
-                    <audio src={heater_3_Sound} className="clip" id='S' ref={S}>
-                        Your browser does not support this type of file.  
-                    </audio>
-                </button>
+                        <button id="Heater 3" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px magenta"}}>
+                            S
+                            <audio src={heater_3_Sound} className="clip" id='S' ref={S}>
+                                Your browser does not support this type of file.  
+                            </audio>
+                        </button>
 
-                <button id="Heater 4" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px orange"}}>
-                    D
-                    <audio src={heater_4_Sound} className="clip" id='D' ref={D}>
-                        Your browser does not support this type of file.  
-                    </audio>
-                </button>
+                        <button id="Heater 4" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px orange"}}>
+                            D
+                            <audio src={heater_4_Sound} className="clip" id='D' ref={D}>
+                                Your browser does not support this type of file.  
+                            </audio>
+                        </button>
+                    </div>
 
-                <button id="Kick n'Hat" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px turquoise"}}>
-                    Z
-                    <audio src={kick_n_hat_Sound} className="clip" id='Z' ref={Z}>
-                        Your browser does not support this type of file.  
-                    </audio>
-                </button>
+                    <div className="row justify-content-center"> 
+                        <button id="Kick n'Hat" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px turquoise"}}>
+                            Z
+                            <audio src={kick_n_hat_Sound} className="clip" id='Z' ref={Z}>
+                                Your browser does not support this type of file.  
+                            </audio>
+                        </button>
 
-                <button id="Kick" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px yellow"}}>
-                    X
-                    <audio src={kick_Sound} className="clip" id='X' ref={X} volume="0.9">
-                        Your browser does not support this type of file.  
-                    </audio>
-                </button>
+                        <button id="Kick" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px yellow"}}>
+                            X
+                            <audio src={kick_Sound} className="clip" id='X' ref={X} volume="0.9">
+                                Your browser does not support this type of file.  
+                            </audio>
+                        </button>
 
-                <button id="Open HH" className="drum-pad col btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px yellowgreen"}}>
-                    C
-                    <audio src={open_HH_Sound} className="clip" id='C' ref={C}>
-                        Your browser does not support this type of file.   
-                    </audio>
-                </button>
+                        <button id="Open HH" className="drum-pad col-4 btn btn-dark m-2" onClick={playSound} style={{boxShadow: "2px 5px yellowgreen"}}>
+                            C
+                            <audio src={open_HH_Sound} className="clip" id='C' ref={C}>
+                                Your browser does not support this type of file.   
+                            </audio>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
