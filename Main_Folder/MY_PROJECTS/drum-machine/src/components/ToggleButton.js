984f10c0eaefc67767a7b1779fa51711cc626d01
CarlG <carl91y@gmail.com>
2022-11-16 15:09:02 -0800

    added switch button

--- Main_Folder/MY_PROJECTS/drum-machine/src/components/ToggleButton.js
+++ Main_Folder/MY_PROJECTS/drum-machine/src/components/ToggleButton.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 export default function ToggleButton(){
-    const[toggle,setToggle]=(false);
+    const[toggle,setToggle]=useState(false);
 
-    const onCLick=()=>{
+    const switchToggle=()=>{
         if (toggle){
             setToggle(false);
         }else{
@@ -13,12 +13,23 @@ export default function ToggleButton(){
 
     
 
-    return(
-        <div className="container-fluid col-2">
-        <div className="row border border-dark col-12 m-0 p0" style={{minHeight:'25px'}}>
-            <div className="col col-6 border border-dark m-0 p-0 bg-primary">
+    if(toggle){
+        return(
+        <div className="container-fluid col-2 border border-dark">
+        <div className="row  col-12 m-0 p0 justify-content-start" style={{minHeight:'25px'}}>
+            <button className="col col-6  m-0 p-0 bg-primary border border-0" onClick={switchToggle}>
+            </button>
+        </div>
+        </div>
+        )
+    }else{
+        return(
+            <div className="container-fluid col-2 border border-dark">
+            <div className="row  col-12 m-0 p0 justify-content-end" style={{minHeight:'25px'}}>
+                <button className="col col-6 m-0 p-0 bg-primary border border-0" onClick={switchToggle}>
+                </button>
             </div>
-        </div>
-        </div>
-    )
+            </div>
+        )
+}
 }
