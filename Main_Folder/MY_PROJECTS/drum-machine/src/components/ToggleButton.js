1b13f1d7746f283c6c20cf68376f3c92030a7afc
CarlG <carl91y@gmail.com>
2022-11-22 16:09:59 -0800

    worked on poitioning

--- Main_Folder/MY_PROJECTS/drum-machine/src/components/ToggleButton.js
+++ Main_Folder/MY_PROJECTS/drum-machine/src/components/ToggleButton.js
@@ -16,20 +16,16 @@ export default function ToggleButton(props){
 
     if(props.toggle){
         return(
-        <div className="container-fluid col-2 border border-dark border-0 ms-5 mt-3 bg-warning" style={{maxHeight:'25px'}}>
-        <div className="row col-12 m-0 p0 justify-content-start" style={{minHeight:'25px'}} >
+        <div className="row m-0 p-0 border border-dark border-0 ms-5 mt-3 bg-warning justify-content-start" style={{minHeight:'25px'}}>
             <button className="col col-6  m-0 p-0 border border-0 bg-primary" onClick={switchToggle}>
             </button>
-        </div>
         </div>
         )
     }else{
         return(
-            <div className="container-fluid col-2 border border-dark border-0 ms-5 mt-3 bg-warning" style={{maxHeight:'25px'}}>
-            <div className="row  col-12 m-0 p0 justify-content-end" style={{minHeight:'25px'}}>
+            <div className="row  m-0 p-0 border border-dark border-0 ms-5 mt-3 bg-warning justify-content-end" style={{minHeight:'25px'}}>
                 <button className="col col-6 m-0 p-0 bg-primary border border-0" onClick={switchToggle}>
                 </button>
-            </div>
             </div>
         )
 }
