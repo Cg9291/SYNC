eae6f190963c749591e0d93184e238830677d9e9
CG <105956982+Cg9291@users.noreply.github.com>
2022-12-19 19:59:07 -0800

    worked on improving randomization

--- Main_Folder/MY_PROJECTS/random-quote-gen/src/redux/components/QuoteBox.js
+++ Main_Folder/MY_PROJECTS/random-quote-gen/src/redux/components/QuoteBox.js
@@ -20,7 +20,7 @@ export default function QuoteBox(props){
     const colorID=useSelector((state)=>state.quotePickerReducer.color);
 
     
-    const dispatch=useDispatch()
+    const dispatch=useDispatch();
     
     const [quoteText,setQuoteText]=useState(quotes[quoteID].quoteText);
     const [authorText,setAuthorText]=useState("- "+quotes[quoteID].quoteAuthor);
@@ -29,20 +29,26 @@ export default function QuoteBox(props){
     
     const handleClick=()=>{
         dispatch(quoteAction());
-        stateTrackingObject.quotes.push(quoteID);
+        if(stateTrackingObject.quotes.indexOf(quoteID)>=0){
+            dispatch(quoteAction());
+        }
+        else{
+            stateTrackingObject.quotes.push(quoteID);
+        }
+        
     }
 
     useEffect(()=>{
         setQuoteText(quotes[quoteID].quoteText);
         setAuthorText(quotes[quoteID].quoteAuthor);
-        setThemeColor(themeColors[colorID])
-    },[quoteID,colorID])
+        setThemeColor(themeColors[colorID]);
+        },[quoteID,colorID])
 
     return(
         <div id="wrapper" style={{backgroundColor:themeColor}}>
             <div id="quote-box">
                 <div id="text" style={{color:themeColor}}> 
-                    {JSON.stringify(stateTrackingObject.quotes)}
+                    {JSON.stringify(stateTrackingObject.quotes)}{/*used for randomness tracking}*/}
                     {quoteText} 
                 </div>
                 <div id="author" style={{color:themeColor}}>
@@ -51,7 +57,7 @@ export default function QuoteBox(props){
                 <div id="footer-row">
                     <div>
                         <a id="tweet-quote" className="bg-sync" target="_top" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22An%20unexamined%20life%20is%20not%20worth%20living.%22%20Socrates" style={{backgroundColor:themeColor}}>
-                            <FontAwesomeIcon icon={faTwitter} />
+                            <FontAwesomeIcon icon={faTwitter}/>
                         </a>
                         <a id="tumblr-quote" className="bg-sync" target="_blank" href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Stephen%20Covey&content=I%20am%20not%20a%20product%20of%20my%20circumstances.%20I%20am%20a%20product%20of%20my%20decisions.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" style={{backgroundColor:themeColor}}>
                             <FontAwesomeIcon icon={faTumblr}/>
--- Main_Folder/MY_PROJECTS/random-quote-gen/src/redux/functions/quotePicker.js
+++ Main_Folder/MY_PROJECTS/random-quote-gen/src/redux/functions/quotePicker.js
@@ -1,9 +1,8 @@
 import stateTrackingObject from "../objects/stateTracking.js";
+
+
 let quotePicker=()=>{
     let quoteIndex=Math.floor(Math.random()*(12-0)+0);
-    if(stateTrackingObject.quotes.indexOf(quoteIndex)>=0){
-        quoteIndex=Math.floor(Math.random()*(12-0)+0);
-    };
-    return quoteIndex;
+        return quoteIndex;
 }
 export default quotePicker;
