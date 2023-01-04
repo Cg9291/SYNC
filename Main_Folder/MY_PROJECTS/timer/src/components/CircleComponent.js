02c5896daa22c962d1ff0da05902918806c61389
CG <carl91y@gmail.com>
2023-01-03 20:14:17 -0800

    Revert "fixed some margins and removed test area"
    This reverts commit 60ee2de881848776abd74c0086cd9ffc9e56fb6e.

--- Main_Folder/MY_PROJECTS/timer/src/components/CircleComponent.js
+++ Main_Folder/MY_PROJECTS/timer/src/components/CircleComponent.js
@@ -5,6 +5,32 @@ export default function Circle(props) {
       <div id="time-left" style={props.timerColorState}>
         {props.minutesRendererFunction()}:{props.secondsRendererFunction()}
       </div>
+      <div id="test-area">
+        <div
+          className="test"
+          style={{ backgroundColor: "red", gridArea: "ar1" }}
+        >
+          uno
+        </div>
+        <div
+          className="test"
+          style={{ backgroundColor: "green", gridArea: "ar2" }}
+        >
+          dos
+        </div>
+        <div
+          className="test"
+          style={{ backgroundColor: "blue", gridArea: "ar3" }}
+        >
+          tres
+        </div>
+        <div
+          className="test"
+          style={{ backgroundColor: "yellow", gridArea: "ar4" }}
+        >
+          quatro
+        </div>
+      </div>
     </div>
   );
 }
