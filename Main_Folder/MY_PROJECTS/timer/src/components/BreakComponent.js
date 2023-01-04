175512149d9288af0aa073900d4543fc28a08246
CG <carl91y@gmail.com>
2023-01-03 20:17:51 -0800

    Revert "moved settings buttons to right side+added margins on them"
    This reverts commit ecd046996807e8f9e772ad0d6f776be5eecdfd93.

--- Main_Folder/MY_PROJECTS/timer/src/components/BreakComponent.js
+++ Main_Folder/MY_PROJECTS/timer/src/components/BreakComponent.js
@@ -6,7 +6,7 @@ export default function Break(props){
                 Length
             </div>
             <button id="break-increment" className="fa fa-arrow-circle-up settings-buttons" onClick={props.breakHandlerFunction}></button>
-            <div id="break-length">{props.breakLengthState}</div>
+            <div id="break-length" className="settings-buttons">{props.breakLengthState}</div>
             <button id="break-decrement" className="fa fa-arrow-circle-down settings-buttons" onClick={props.breakHandlerFunction}></button> 
         </div>
     );
