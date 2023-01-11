fa106f50e755e6bec18bf531fbbd8da405a9191a
CarlG <carl91y@gmail.com>
2023-01-11 15:18:10 -0800

    implemented desktop/laptop responsive design

--- Main_Folder/MY_PROJECTS/timer/src/components/BreakComponent.js
+++ Main_Folder/MY_PROJECTS/timer/src/components/BreakComponent.js
@@ -1,12 +1,33 @@
-export default function Break(props){
-    return(
-        <div id='break' className="time-modifiers">
-            <div id='break-label' className="labels">
-                Break Length
-            </div>
-            <button id="break-increment" className="fa fa-arrow-circle-up settings-buttons increment-buttons" onClick={props.breakHandlerFunction}></button>
-            <div id="break-length" className="length-values">{props.breakLengthState}</div>
-            <button id="break-decrement" className="fa fa-arrow-circle-down settings-buttons decrement-buttons" onClick={props.breakHandlerFunction}></button> 
+export default function Break(props) {
+  return (
+    <div
+      id="break"
+      className="time-modifiers"
+    >
+      <div
+        id="break-label"
+        className="labels"
+      >
+        Break Length
+      </div>
+      <div className="btn-row">
+        <button
+          id="break-increment"
+          className="fa fa-arrow-circle-up fa-4x settings-buttons increment-buttons"
+          onClick={props.breakHandlerFunction}
+        ></button>
+        <div
+          id="break-length"
+          className="length-values"
+        >
+          {props.breakLengthState}
         </div>
-    );
+        <button
+          id="break-decrement"
+          className="fa fa-arrow-circle-down fa-4x settings-buttons decrement-buttons"
+          onClick={props.breakHandlerFunction}
+        ></button>
+      </div>
+    </div>
+  );
 }
--- Main_Folder/MY_PROJECTS/timer/src/components/SessionComponent.js
+++ Main_Folder/MY_PROJECTS/timer/src/components/SessionComponent.js
@@ -1,20 +1,33 @@
 export default function Session(props) {
   return (
-    <div id='session' className="time-modifiers">
-      <div id="session-label" className="labels">
+    <div
+      id="session"
+      className="time-modifiers"
+    >
+      <div
+        id="session-label"
+        className="labels"
+      >
         Session Length
       </div>
-      <button
-        id="session-increment"
-        className="fa fa-arrow-circle-up increment-buttons settings-buttons "
-        onClick={props.sessionHandlerFunction}
-      ></button>
-      <div id="session-length" className="length-values">{props.sessionLengthState}</div>
-      <button
-        id="session-decrement"
-        className="fa fa-arrow-circle-down decrement-buttons settings-buttons"
-        onClick={props.sessionHandlerFunction}
-      ></button>
+      <div className="btn-row">
+        <button
+          id="session-increment"
+          className="fa fa-arrow-circle-up fa-4x increment-buttons settings-buttons "
+          onClick={props.sessionHandlerFunction}
+        ></button>
+        <div
+          id="session-length"
+          className="length-values"
+        >
+          {props.sessionLengthState}
+        </div>
+        <button
+          id="session-decrement"
+          className="fa fa-arrow-circle-down fa-4x decrement-buttons settings-buttons"
+          onClick={props.sessionHandlerFunction}
+        ></button>
+      </div>
     </div>
   );
 }
