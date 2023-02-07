0957db9a81917bbb99d40685fc6380ec6e3b906d
CarlG <carl91y@gmail.com>
2023-02-07 14:36:51 -0800

    converted Midnight Sillage kit from aif format to mp3(using software),now playing

--- Main_Folder/MY_PROJECTS/drum-machine/src/components/Buttons.js
+++ Main_Folder/MY_PROJECTS/drum-machine/src/components/Buttons.js
@@ -1,17 +1,22 @@
 //MUST REPLACE KICK!!! (FILE DOESNT WORK)
 //OPTIONAL IMPROVEMENTS:(1-CHANGE KEYS COLOR WHEN PRESSED/CLICKED 2-ADD WINDOWS FOR LABEL AND POWER ON/OFF 3-FIX UNEVEN VOLUME)
+/*had to convert Midnight Sillage Kit (logic macbook sounds) from aif to mp3 using online converter*/
+
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import ToggleButton from "./ToggleButton.js";
 import "../buttons.scss";
-import clap_Sound from "../audio/clap.mp3";
-import closed_HH_Sound from "../audio/closed-HH.mp3";
-import heater_1_Sound from "../audio/heater-1.mp3";
-import heater_2_Sound from "../audio/heater-2.mp3";
-import heater_3_Sound from "../audio/heater-3.mp3";
-import heater_4_Sound from "../audio/heater-4.mp3";
-import kick_n_hat_Sound from "../audio/kick_n_Hat.mp3";
-import kick_Sound from "../audio/kick.mp3";
-import open_HH_Sound from "../audio/open - HH.mp3";
+import clap_Sound from "../audio/basic sounds/clap.mp3";
+import closed_HH_Sound from "../audio/basic sounds/closed-HH.mp3";
+import heater_1_Sound from "../audio/basic sounds/heater-1.mp3";
+import heater_2_Sound from "../audio/basic sounds/heater-2.mp3";
+import heater_3_Sound from "../audio/basic sounds/heater-3.mp3";
+import heater_4_Sound from "../audio/basic sounds/heater-4.mp3";
+import kick_n_hat_Sound from "../audio/basic sounds/kick_n_Hat.mp3";
+import kick_Sound from "../audio/basic sounds/kick.mp3";
+import open_HH_Sound from "../audio/basic sounds/open - HH.mp3";
+import sax from "../audio/Midnight Sillage Kit/Sax_Midnight_Sillage.mp3"
+import electric_guitar_01 from "../audio/Midnight Sillage Kit/Electric Guitar 01 - Midnight Sillage.aif"
+import kick from "../audio/Midnight Sillage Kit/Electric Piano 01 - Midnight Sillage.mp3"
 
 export default function Buttons() {
   const buttonRefs = useRef([]); //using REFCALLBACK to target specific node/element
@@ -161,7 +166,7 @@ export default function Buttons() {
             >
               Q
               <audio
-                src={clap_Sound}
+                src={sax}
                 className="clip"
                 id="Q"
                 ref={ref => (audioRefs.current.Q = ref)}
@@ -179,7 +184,7 @@ export default function Buttons() {
             >
               W
               <audio
-                src={closed_HH_Sound}
+                src={kick}
                 className="clip"
                 id="W"
                 ref={ref => (audioRefs.current.W = ref)}
