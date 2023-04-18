144b6b2c236ec5255181879258cd0451d1c3db69
CarlG <carl91y@gmail.com>
2023-04-18 16:19:02 -0700

    cleaned up some css & cleaned up rock paper scissors app

--- Main_Folder/MY_PROJECTS/rock_paper_scissors/index.js
+++ Main_Folder/MY_PROJECTS/rock_paper_scissors/index.js
@@ -1,25 +1,24 @@
 /*
 TODO
+*fix game logic in choice functions
+*figure out if userChoice function should be broken down
 -replace & import missing images(broken links causing issues)
 -get correct image sizing
 -make images responsive
--get design with Anais
+*get design with Anais
 
 */
 
-let rockImg =
-  "./images/the-rock-flex.jpg";
+let rockImg = "./images/the-rock-flex.jpg";
 
-let paperImg =
-  "./images/toilet-paper.jpg";
+let paperImg = "./images/toilet-paper.jpg";
 
-let scissorsImg =
-  "./images/caesars.jpg";
+let scissorsImg = "./images/caesars.jpg";
 
 document.getElementById("user-img").width = 405;
 document.getElementById("cpu-img").width = 405;
 
-let numRef = [rockImg, paperImg, scissorsImg];
+let choicesArray = [rockImg, paperImg, scissorsImg];
 
 let round = 0;
 
@@ -40,18 +39,22 @@ function userChoice(id) {
       break;
   }
 
-  let randomNum = Math.floor(Math.random() * 3);
+  let cpuRandomNum = Math.floor(Math.random() * 3);
 
-  document.getElementById("cpu-img").src = numRef[randomNum];
+  document.getElementById("cpu-img").src = choicesArray[cpuRandomNum];
 
   round++;
 
+  let userChoiceIndex = choicesArray.indexOf(result);
+
   if (
-    (numRef.indexOf(result) == numRef.length - 1 && randomNum == 0) ||
-    numRef.indexOf(result) < randomNum
+    userChoiceIndex<cpuRandomNum && (userChoiceIndex!==choicesArray.length-1 && cpuRandomNum!==0)
   ) {
     document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Lost!!"; //loss
-  } else if (numRef.indexOf(result) > randomNum) {
+  } else if (
+    userChoiceIndex > cpuRandomNum ||
+    (userChoiceIndex == 0 && cpuRandomNum == choicesArray.length - 1)
+  ) {
     document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Win!!"; //win
   } else {
     document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "It's a tie!!"; //tie
@@ -59,16 +62,20 @@ function userChoice(id) {
 }
 
 function randomChoice() {
-  let randomNum = Math.floor(Math.random() * 3);
-  let randomNumCpu = Math.floor(Math.random() * 3);
+  let userRandomNum = Math.floor(Math.random() * 3);
+  let cpuRandomNum = Math.floor(Math.random() * 3);
+  /* can have the same name as the one in userChoice function because they are both block scoped */
 
-  document.getElementById("user-img").src = numRef[randomNum];
-  document.getElementById("cpu-img").src = numRef[randomNumCpu];
+  document.getElementById("user-img").src = choicesArray[userRandomNum];
+  document.getElementById("cpu-img").src = choicesArray[cpuRandomNum];
   round++;
 
-  if ((randomNum == numRef.length - 1 && randomNumCpu == 0) || randomNum < randomNumCpu) {
+  if (
+    (userRandomNum == choicesArray.length - 1 && cpuRandomNum == 0) ||
+    userRandomNum < cpuRandomNum
+  ) {
     document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Lost!!"; //loss
-  } else if (randomNum > randomNumCpu) {
+  } else if (userRandomNum > cpuRandomNum) {
     document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Win!!"; //win
   } else {
     document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "It's a tie!!"; //tie
