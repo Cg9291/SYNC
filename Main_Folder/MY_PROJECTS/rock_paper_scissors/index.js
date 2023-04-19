d87136cc1ee59b776b928ad570010fb957149338
CarlG <carl91y@gmail.com>
2023-04-19 12:03:48 -0700

    fixed feedback upon user choice or random selection click

--- Main_Folder/MY_PROJECTS/rock_paper_scissors/index.js
+++ Main_Folder/MY_PROJECTS/rock_paper_scissors/index.js
@@ -22,6 +22,15 @@ let choicesArray = [rockImg, paperImg, scissorsImg];
 
 let round = 0;
 
+let winFeedback = () =>
+  (document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Win!!");
+
+let lossFeedback = () =>
+  (document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Lost!!");
+
+let tieFeedback = () =>
+  (document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "It's a tie!!");
+
 function userChoice(id) {
   let result = 0;
   switch (id) {
@@ -47,18 +56,61 @@ function userChoice(id) {
 
   let userChoiceIndex = choicesArray.indexOf(result);
 
-  if (
-    userChoiceIndex<cpuRandomNum && (userChoiceIndex!==choicesArray.length-1 && cpuRandomNum!==0)
-  ) {
-    document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Lost!!"; //loss
-  } else if (
-    userChoiceIndex > cpuRandomNum ||
-    (userChoiceIndex == 0 && cpuRandomNum == choicesArray.length - 1)
-  ) {
-    document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Win!!"; //win
-  } else {
-    document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "It's a tie!!"; //tie
+  switch (userChoiceIndex) {
+    case 0 /* picking Rock */:
+      switch (cpuRandomNum) {
+        case 0:
+          tieFeedback();
+          break;
+        case 1:
+          lossFeedback();
+          break;
+        case 2:
+          winFeedback();
+          break;
+        default:
+          alert(`ERROR:returned cpu value of ${cpuRandomNum} should not exist`);
+          break;
+      }
+      break;
+    case 1 /* picking Paper */:
+      switch (cpuRandomNum) {
+        case 0:
+          winFeedback();
+          break;
+        case 1:
+          tieFeedback();
+          break;
+        case 2:
+          lossFeedback();
+          break;
+        default:
+          alert(`ERROR:returned cpu value of ${cpuRandomNum} should not exist`);
+          break;
+      }
+      break;
+    case 2 /* picking Scissors */:
+      switch (cpuRandomNum) {
+        case 0:
+          lossFeedback();
+          break;
+        case 1:
+          winFeedback();
+          break;
+        case 2:
+          tieFeedback();
+          break;
+        default:
+          alert(`ERROR:returned cpu value of ${cpuRandomNum} should not exist`);
+          break;
+      }
+      break;
+
+    default:
+      alert(`ERROR:returned user value of ${cpuRandomNum} should not exist`);
+      break;
   }
+ 
 }
 
 function randomChoice() {
@@ -70,14 +122,58 @@ function randomChoice() {
   document.getElementById("cpu-img").src = choicesArray[cpuRandomNum];
   round++;
 
-  if (
-    (userRandomNum == choicesArray.length - 1 && cpuRandomNum == 0) ||
-    userRandomNum < cpuRandomNum
-  ) {
-    document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Lost!!"; //loss
-  } else if (userRandomNum > cpuRandomNum) {
-    document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "You Win!!"; //win
-  } else {
-    document.getElementById("results").innerHTML = "Round#: " + round + "<br>" + "It's a tie!!"; //tie
+  switch (userRandomNum) {
+    case 0 /* picking Rock */:
+      switch (cpuRandomNum) {
+        case 0:
+          tieFeedback();
+          break;
+        case 1:
+          lossFeedback();
+          break;
+        case 2:
+          winFeedback();
+          break;
+        default:
+          alert(`ERROR:returned cpu value of ${cpuRandomNum} should not exist`);
+          break;
+      }
+      break;
+    case 1 /* picking Paper */:
+      switch (cpuRandomNum) {
+        case 0:
+          winFeedback();
+          break;
+        case 1:
+          tieFeedback();
+          break;
+        case 2:
+          lossFeedback();
+          break;
+        default:
+          alert(`ERROR:returned cpu value of ${cpuRandomNum} should not exist`);
+          break;
+      }
+      break;
+    case 2 /* picking Scissors */:
+      switch (cpuRandomNum) {
+        case 0:
+          lossFeedback();
+          break;
+        case 1:
+          winFeedback();
+          break;
+        case 2:
+          tieFeedback();
+          break;
+        default:
+          alert(`ERROR:returned cpu value of ${cpuRandomNum} should not exist`);
+          break;
+      }
+      break;
+
+    default:
+      alert(`ERROR:returned user value of ${cpuRandomNum} should not exist`);
+      break;
   }
 }
