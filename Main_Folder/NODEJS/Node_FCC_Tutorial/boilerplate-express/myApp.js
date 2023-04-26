6408050eeeb35cff94fc69c8c13e6d0393ff35b9
CarlG <carl91y@gmail.com>
2023-04-26 14:42:56 -0700

    finished fcc node intro course

--- Main_Folder/NODEJS/Node_FCC_Tutorial/boilerplate-express/myApp.js
+++ Main_Folder/NODEJS/Node_FCC_Tutorial/boilerplate-express/myApp.js
@@ -1,19 +1,89 @@
 //MAKE SURE TO NEVER PUSH ENV FILE TO GITHUB, ADD IT TO GIT IGNORE LIST
-require('dotenv').config({path:'SYNC/JAVASCRIPT_CODE/NODEJS/Node FCC Tutorial/boilerplate-express/.env'});
-let bodyParser=require('body-parser');
+
+
 //FCC Q# [Use the .env File]=>Biggest hurdle here was:
 //1-not being able to access env vars unless in the root(programming folder//non project root)
 //2- not being able to use comparison operator even able to access env vars through non project root
 //3- had to read documentation to be able to add path in config up there
 var livereload = require("livereload");
+//TO PARSE POST REQUEST USING A BODYPARSER 1/2
+let bodyParser = require("body-parser");
 var connectLiveReload = require("connect-livereload");
 
-let express = require('express');
-const { json } = require('body-parser');
+let express = require("express");
+const {json} = require("body-parser");
 let app = express();
+require("dotenv").config();
 
 console.log("Hello World");
-const players={
+
+//TO PARSE POST REQUEST USING A BODYPARSER 2/2 + MIDDLEWARE TO PARSE URL ENCODED DATA(FOR POST REQUESTS MOSTLY ..IE /MYPATH?MYPROP=MYVAL&MYPROP2=MYVAL2)
+app.use(bodyParser.urlencoded({extended: false}));
+
+// TO SERVE STATIC ASSETS/FILES 1/2
+app.use("/public", express.static(__dirname + "/public"));
+
+//TO CREATE A REQUEST LOGGER MIDDLEWARE
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
+//TO CHAIN A MIDDLEWARE AND CREATE TIME SERVER
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		next();
+	},
+	(req, res) => {
+		res.json({time: req.time});
+	}
+);
+
+//TO GET SPECIFIC REQUEST INFORMATION FROM CLIENT USING ROUTE PARAMETERS(/MYPATH/MYPROP/:MYVAL)
+app.get("/:word/echo", (req, res) => {
+	res.json({echo: req.params.word});
+});
+
+
+//TO GET SPECIFIC REQUEST INFORMATION FROM CLIENT USING QUERY PARAMETERS(/MYPATH?MYPROP=MYVAL&MYPROP2=MYVAL2)
+app.get("/name", (req, res) => {
+	res.json({name:`${req.query.first} ${req.query.last}`});
+});
+
+//TO POST DATA TO API & HAVE IT DISPLAYED(FORM & JSON..SEE INDEX.HTML FILE FOR POSTING METHOD)
+app.post("/name",(req,res)=>{
+    res.json({name:`${req.body.first} ${req.body.last}`});
+})
+
+/* // TO SERVE STRINGS
+app.get("/",function(req,res){
+    res.send('Hello Express');
+}) */
+
+/* // TO SERVE FILES
+ app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+}) */
+
+// TO SERVE STATIC ASSETS/FILES 2/2
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/index.html");
+});
+
+// TO SERVE JSON AND ACCESS .ENV FILES
+app.get("/json", (req, res) => {
+	if (process.env.MESSAGE_STYLE == "uppercase") {
+		res.json({message: "Hello json".toUpperCase()});
+	} else {
+		res.json({message: "Hello json"});
+	}
+});
+
+//OLD
+/* const players={
     davis:{//0
         Lastname: "Davis",
         Firstname: "Anthony",
@@ -75,10 +145,9 @@ const players={
         Jerseynumber:10,
         Height:198}
 };
+ */
 
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.use((req,res,next)=>{//could not submit,works it terminal but not when submitted on fcc, found multiple posts pointing to error on their end..will follow up
+/* app.use((req,res,next)=>{//could not submit,works it terminal but not when submitted on fcc, found multiple posts pointing to error on their end..will follow up
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 });
@@ -105,7 +174,7 @@ app.get("/name", function(req,res) {
     var firstName = req.query.first;
     var lastName = req.query.last;
     // OR you can destructure and rename the keys
-   
+
     // Use template literals to form a formatted string
     res.json({
       name: `${firstName} ${lastName}`
@@ -114,7 +183,7 @@ app.get("/name", function(req,res) {
 
   //personal example
   app.get("/players", function(req,res) {
-   
+
     res.json({
       name: `${firstName} ${lastName}`
     });
@@ -122,10 +191,7 @@ app.get("/name", function(req,res) {
 
   app.post('/name',(req,res)=>{
     res.json({name:`${req.body.first} ${req.body.last}`})
-  })
-
-    
-
+  }) */
 /*
 app.post('/name',
     app.use(bodyParser.urlencoded({extended: false}
@@ -150,7 +216,7 @@ app.get("/",(req,res)=>{
     app.use("/public",express.static(__dirname + '/public'));
 })
 
-app.get("/json",(req,res)=>{  
+app.get("/json",(req,res)=>{
     if(process.env.MESSAGE_STYLE=="uppercase"){
         res.json({"message": "HELLO JSON"})
     }
@@ -170,37 +236,4 @@ app.get("/now",(req,res,next)=>{
 )
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
