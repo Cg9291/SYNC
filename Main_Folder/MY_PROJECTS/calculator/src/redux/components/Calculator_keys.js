14dda992ea0f86da6bc101d3e9b79171bf4faaff
CarlG <carl91y@gmail.com>
2023-05-05 14:19:37 -0700

    fully finished button positioning/layout

--- Main_Folder/MY_PROJECTS/calculator/src/redux/components/Calculator_keys.js
+++ Main_Folder/MY_PROJECTS/calculator/src/redux/components/Calculator_keys.js
@@ -68,47 +68,50 @@ export const Keys = forwardRef(function Keys(props, btnRef) {
 						optionalClasses=""
 					/>
 				</div>
-
-				<div className="row  p-0 m-0 h-20 border justify-content-evenly">
-					<NumberKeys
-						id="one"
-						value={1}
-						optionalClasses=""
-					/>
-					<NumberKeys
-						id="two"
-						value={2}
-						optionalClasses=""
-					/>
-					<NumberKeys
-						id="three"
-						value={3}
-						optionalClasses=""
-					/>
-
-					<div className="m-0 p-0 col-3 border border-danger ">
-						<OperatorKeys
-							id="equals"
-							value="="
-							optionalClasses="border  w-100 h-200 mt-2 "
-						/>
-
+				<div className="row m-0 p-0 h-40 border">
+					<div className="col-9 m-0 p-0 h-100">
+						<div className="row p-0 m-0 h-50 border justify-content-evenly">
+							<NumberKeys
+								id="one"
+								value={1}
+								optionalClasses=" col-4 m-0 p-0"
+							/>
+							<NumberKeys
+								id="two"
+								value={2}
+								optionalClasses="col-4 m-0 p-0"
+							/>
+							<NumberKeys
+								id="three"
+								value={3}
+								optionalClasses="col-4 m-0 p-0"
+							/>
+						</div>
+						<div className="row p-0 m-0 h-50  justify-content-evenly">
+							<NumberKeys
+								id="zero"
+								value={0}
+								optionalClasses="col-8"
+							/>
+							<button
+								id="decimal"
+								className="col-4 btn btn-secondary shadow-none rounded   h-80 my-auto"
+								value={"."}
+								onClick={handlers.handleDecimalClick}
+							>
+								.
+							</button>
+						</div>
 					</div>
-				</div>
-				<div className="row w-75 p-0 m-0 h-20  border ">
-					<NumberKeys
-						id="zero"
-						value={0}
-						optionalClasses="col-8"
-					/>
-					<button
-						id="decimal"
-						className="col-4 btn btn-secondary shadow-none rounded border  h-80 my-auto"
-						value={"."}
-						onClick={handlers.handleDecimalClick}
-					>
-						.
-					</button>
+					<div className="m-0 p-0 col-3 h-100 ">
+						<div className="row m-0 p-0 h-100  ">
+							<OperatorKeys
+								id="equals"
+								value="="
+								optionalClasses=" w-100 h-90 "
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 			{/* <div className="d-flex flex-column w-25 h-100 m-0 p-0 ">
