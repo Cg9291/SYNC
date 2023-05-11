eebac1244f839e69105ae15b891d7d919b041e39
CG <carl91y@gmail.com>
2023-05-10 19:00:47 -0700

    added svg"s and aligned them in nav row -mobile

--- Main_Folder/MY_PROJECTS/twitter_clone/src/components/Navigation.js
+++ Main_Folder/MY_PROJECTS/twitter_clone/src/components/Navigation.js
@@ -1,23 +1,43 @@
-import { useState } from "react"
+import {useState} from "react";
+import {homeIcon, searchIcon,bellIcon,messageIcon} from "./svg_exports";
 
-export default function Navigation(props){
-    const [li1,setLi1]=useState("Home")
-    return (
-      <nav>
-        <ul>
-         {/*  <img id="twitter-logo"
-        src="https://raw.githubusercontent.com/gist/mbostock/3094619/raw/78116ff0306b3b5c3f40e6cdd5f6f8f648ecffd1/thumbnail.png"
-        alt="twitter logo" /> */}
-          <a href="#"><li><b>{li1}</b></li></a>
-          <a href="#"><li>Explore</li></a>
-          <a href="#"><li>Notifications</li></a>
-          <a href="#"><li>Messages</li></a>
-          <a href="#"><li>Bookmarks</li></a>
-          <a href="#"> <li>Lists</li></a>
-          <a href="#"><li>Profile</li></a>
-          <a href="#"><li>More</li></a>
-          <a href="#"><button type="click" /*onClick={()=>setLi1("Casa")}*/>Tweet</button></a>
-        </ul>
-      </nav>
-    )
-  }
+export default function Navigation(props) {
+	return (
+		<nav>
+			<ul>
+				<a href="#">
+					<li>
+						<b>{homeIcon}</b>
+					</li>
+				</a>
+				<a href="#">
+					<li>
+						<b>{searchIcon}</b>
+					</li>
+				</a>
+				<a href="#">
+					<li>{bellIcon}</li>
+				</a>
+				<a href="#">
+					<li>{messageIcon}</li>
+				</a>
+				{/* <a href="#">
+					<li>Bookmarks</li>
+				</a>
+				<a href="#">
+					{" "}
+					<li>Lists</li>
+				</a>
+				<a href="#">
+					<li>Profile</li>
+				</a>
+				<a href="#">
+					<li>More</li>
+				</a>
+				<a href="#">
+					<button type="click" /*onClick={()=>setLi1("Casa")}/>Tweet</button>
+				</a> */}
+			</ul>
+		</nav>
+	);
+}
