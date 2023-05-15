c706d96c65d702dc7c755653ba919687a910a386
CarlG <carl91y@gmail.com>
2023-05-15 14:38:42 -0700

    removed lots of unused css code

--- Main_Folder/MY_PROJECTS/twitter_clone/src/components/Timeline.js
+++ Main_Folder/MY_PROJECTS/twitter_clone/src/components/Timeline.js
@@ -15,13 +15,30 @@ export default function Timeline(props) {
 			>
 				A Twitter List by TheSportsMediaC
 			</a>
+
+			<script
+				async
+				src="https://platform.twitter.com/widgets.js"
+				charset="utf-8"
+			></script>
+			{/* <a
+				id="timeline"
+				class="twitter-timeline"
+				href="https://twitter.com/TheSportsMediaC/lists/1543020161199812608?ref_src=twsrc%5Etfw"
+				data-chrome="noheader"
+				sourceType="list"
+				ownerScreenName="TwitterDev"
+				slug="national-parks"
+			>
+				A Twitter List by TheSportsMediaC
+			</a>
 			<Helmet>
 				<script
 					async
 					src="https://platform.twitter.com/widgets.js"
 					charset="utf-8"
 				></script>
-			</Helmet>
+			</Helmet> */}
 		</div>
 	);
 }
