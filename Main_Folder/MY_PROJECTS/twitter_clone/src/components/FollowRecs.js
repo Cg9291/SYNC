9243bc3eb3002749b4176747bf2798be0a3c01a7
CG <carl91y@gmail.com>
2023-05-20 15:27:49 -0700

    finished layout of follow recs section @width=>988px

--- Main_Folder/MY_PROJECTS/twitter_clone/src/components/FollowRecs.js
+++ Main_Folder/MY_PROJECTS/twitter_clone/src/components/FollowRecs.js
@@ -1,11 +1,13 @@
 export default function FollowsRecSection(){
-    return(
-      <section className="follow-recommendations">
-        <div class="cards-header">Who to follow</div>
-        <div class="cards">PROFILE #1</div>
-        <div class="cards">PROFILE #2</div>
-        <div class="cards">PROFILE #3</div>
-        <div class="cards-footer"><a href='#'>Show more</a></div>
-      </section>
-    )
+    return (
+			<section className="follow-recommendations">
+				<div class="follow-recommendations_cards-header">Who to follow</div>
+				<div class="follow-recommendations_cards">PROFILE #1</div>
+				<div class="follow-recommendations_cards">PROFILE #2</div>
+				<div class="follow-recommendations_cards">PROFILE #3</div>
+				<div class="follow-recommendations_cards-footer">
+					<a href="#">Show more</a>
+				</div>
+			</section>
+		);
   }
