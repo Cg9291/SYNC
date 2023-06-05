6ed2b29b9e447d9a636be9cb21036dbc962ca264
CarlG <carl91y@gmail.com>
2023-06-05 16:26:09 -0700

    completed major alignments/positioning for width<=1024px + added searchbar details

--- Main_Folder/MY_PROJECTS/twitter_clone/src/components/Searchbar.js
+++ Main_Folder/MY_PROJECTS/twitter_clone/src/components/Searchbar.js
@@ -1,7 +1,16 @@
-export default function SearchBar(props){
-    return (
-			<div className="searchBar-wrapper">
-				<input className="searchBar" placeholder="Search Twitter"></input>
-			</div>
-		);
-  }
+import { searchIcon } from "../assets/icons/svg_exports";
+
+export default function SearchBar(props) {
+	return (
+		<div className="searchBar-wrapper">
+			<label className="searchBar">
+				<div className="searchBar_searchIcon-wrapper">
+					{searchIcon}</div>
+				<input
+					className="searchBar_input"
+					placeholder="Search Twitter"
+				></input>
+			</label>
+		</div>
+	);
+}
