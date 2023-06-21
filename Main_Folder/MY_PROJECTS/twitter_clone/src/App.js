f0e913c00efbca9f15390e83db8275e115923be7
CarlG <carl91y@gmail.com>
2023-06-21 16:28:46 -0700

    updated to dos

--- Main_Folder/MY_PROJECTS/twitter_clone/src/App.js
+++ Main_Folder/MY_PROJECTS/twitter_clone/src/App.js
@@ -29,7 +29,7 @@ TABLETS!!
 *change svg fill on focus
 *add blue color to search bar border+icon on focus/selection
 *review flexible sizing for trending cards(maybe after api is added)
-*review scroll behavior of right container to make it move with timeline up to a point
+*review scroll behavior of right container to make it move with timeline up to a point(need to join mid and third container)
 -for you,following and myfeed could use minor position adjustments
 */
 
