44031c4b73b57a59a758722cd44c4a69aedb4dbb
CG <carl91y@gmail.com>
2023-07-08 10:26:47 -0700

    added wrapper to separare scroll behaviour of nav header & ul vs profile element at bottom

--- Main_Folder/MY_PROJECTS/twitter_clone/src/components/Navigation.js
+++ Main_Folder/MY_PROJECTS/twitter_clone/src/components/Navigation.js
@@ -18,28 +18,29 @@ import {
 export default function Navigation(props) {
 	return (
 		<nav className="nav">
-			<div className="nav_twitter-logo-box">{twitterLogoNav}</div>
-			<ul className="nav_ul">
-				<li className=" nav_li--first-item">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{homeIcon}
-						<span className="nav_text">Home</span>
-					</a>
-				</li>
+			<div className="wrapper">
+				<div className="nav_twitter-logo-box">{twitterLogoNav}</div>
+				<ul className="nav_ul">
+					<li className=" nav_li--first-item">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{homeIcon}
+							<span className="nav_text">Home</span>
+						</a>
+					</li>
 
-				<li className="nav_li--search-icon">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{searchIconNav}
-						<span className="nav_text">Explore</span>
-					</a>
-				</li>
-				{/* <li className="nav_li--hashtag-icon">
+					<li className="nav_li--search-icon">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{searchIconNav}
+							<span className="nav_text">Explore</span>
+						</a>
+					</li>
+					{/* <li className="nav_li--hashtag-icon">
 					<a
 						href="#"
 						className="nav_a"
@@ -48,87 +49,88 @@ export default function Navigation(props) {
 					</a>
 				</li> */}
 
-				<li className="nav_li">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{bellIcon}
-						<span className="nav_text">Notifications</span>
-					</a>
-				</li>
+					<li className="nav_li">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{bellIcon}
+							<span className="nav_text">Notifications</span>
+						</a>
+					</li>
 
-				<li className="nav_li">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{messageIcon}
-						<span className="nav_text">Messages</span>
-					</a>
-				</li>
+					<li className="nav_li">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{messageIcon}
+							<span className="nav_text">Messages</span>
+						</a>
+					</li>
 
-				<li className="nav_li hidden615-h ">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{listsIcon}
-						<span className="nav_text">Lists</span>
-					</a>
-				</li>
+					<li className="nav_li hidden615-h ">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{listsIcon}
+							<span className="nav_text">Lists</span>
+						</a>
+					</li>
 
-				<li className="nav_li hidden615-h">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{bookmarksIcon}
-						<span className="nav_text">Bookmarks</span>
-					</a>
-				</li>
+					<li className="nav_li hidden615-h">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{bookmarksIcon}
+							<span className="nav_text">Bookmarks</span>
+						</a>
+					</li>
 
-				<li className="nav_li hidden500">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{communitiesIcon}
-						<span className="nav_text">Communities</span>
-					</a>
-				</li>
+					<li className="nav_li hidden500">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{communitiesIcon}
+							<span className="nav_text">Communities</span>
+						</a>
+					</li>
 
-				<li className="nav_li hidden500">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{twitterBlueIcon}
-						<span className="nav_text">Verified</span>
-					</a>
-				</li>
+					<li className="nav_li hidden500">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{twitterBlueIcon}
+							<span className="nav_text">Verified</span>
+						</a>
+					</li>
 
-				<li className="nav_li hidden500">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{profileIcon}
-						<span className="nav_text">Profile</span>
-					</a>
-				</li>
+					<li className="nav_li hidden500">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{profileIcon}
+							<span className="nav_text">Profile</span>
+						</a>
+					</li>
 
-				<li className="nav_li hidden500">
-					<a
-						href="#"
-						className="nav_a"
-					>
-						{moreIcon}
-						<span className="nav_text">More</span>
-					</a>
-				</li>
-			</ul>
-			<div className="wrappers2">{tweetButton("Tweet")}</div>
+					<li className="nav_li hidden500">
+						<a
+							href="#"
+							className="nav_a"
+						>
+							{moreIcon}
+							<span className="nav_text">More</span>
+						</a>
+					</li>
+				</ul>
+				<div className="wrappers2">{tweetButton("Tweet")}</div>
+			</div>
 		</nav>
 	);
 }
