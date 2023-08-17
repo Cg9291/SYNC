ae5dd0988746f67df862ecd7164a901ebca1d740
CarlG <carl91y@gmail.com>
2023-08-17 16:30:39 -0700

    worked on algorithms

---
+++ Main_Folder/NODEJS/NODE_DOCUMENTATION_TUTORIALS/FS_Module/basic_functions.js
@@ -0,0 +1,9 @@
+const myObj={
+    name:"cg",
+    age:"30",
+    city:"burnaby",
+}
+
+const{name:prenom,age,city}=myObj;
+
+console.log(age,prenom)
--- Main_Folder/NODEJS/NODE_DOCUMENTATION_TUTORIALS/server.js
+++ Main_Folder/NODEJS/NODE_DOCUMENTATION_TUTORIALS/server.js
@@ -1,10 +1,10 @@
 import http from "http";
 import fs from "fs";
-//import { postToServer } from "./Calls/echo_call.js";
+import { postToServer } from "./Calls/echo_call.js";
 //import html from "./index.html"
 //import { getApiServerData } from "./Calls/lucas_api_call.js";
 
-var body = "Welcome to the echo endpoint";
+/* var body = "Welcome to the echo endpoint";
 export let avar;
 
 const server = http.createServer((req, res) => {
@@ -23,18 +23,18 @@ const server = http.createServer((req, res) => {
 		req
 			.on("data", chunk => {
 				body.push(chunk);
-				//req.pipe(res);
+				req.pipe(res);
 			})
 			.on("end", () => {
 				body = Buffer.concat(body).toString();
 				//console.log(body);
-				res.end(body);
+				res.end();
 			});
 
 		res
 			.writeHead(200, { "Content-Type": "application/javascript" })
 			.on("finish", () => {
-				console.log(body);
+				//console.log(res);
 			});
 	} else if (req.method === "GET" && req.url === "/echo") {
 		//console.log(body);
@@ -54,3 +54,24 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
 	console.log("Server is operational", http.METHODS);
 });
+ */
+
+
+http
+	.createServer((request, response) => {
+		request.on("error", err => {
+			console.error(`error is ${err}`);
+			response.statusCode = 400;
+			response.end();
+		});
+		response.on("error", err => {
+			console.error(err);
+		});
+		if (request.method === "POST" && request.url === "/echo") {
+			request.pipe(response);
+		} else {
+			response.statusCode = 404;
+			response.end(response);
+		}
+	})
+	.listen(3000,()=>{console.log("hey")});
