13fb098e80bcd3d89335ece9b1759a72e1947c3f
CG <carl91y@gmail.com>
2023-08-20 21:43:56 -0700

    started headerparser project

---
+++ Main_Folder/NODEJS/NODE_FCC_PROJECTS/boilerplate-project-headerparser/server.js
@@ -0,0 +1,31 @@
+import http from "http";
+import url from "url";
+
+http
+	.createServer((req, res) => {
+		let myResObj = {};
+
+		req.on("error", err => {
+			console.log(err);
+		});
+
+		res.on("error", err => {
+			console.log(err);
+		});
+
+		if (req.url === "/api/whoami") {
+			myResObj.ipaddress = req.headers.host
+			myResObj.language = req.headers["accept-language"];
+			myResObj.software = req.headers["user-agent"];
+			res.writeHead(200, { "Content-type": "application/json" });
+			res.end(JSON.stringify(myResObj));
+			console.log(req.headers);
+		} else {
+			//res.writeHead(200, { "Content-type": "plain/text" });
+            res.writeHead(200, { "Content-type": "text/plain" });
+			res.end("Welcome to the root");
+		}
+	})
+	.listen(3000, () => {
+		console.log("Server is listening");
+	});
